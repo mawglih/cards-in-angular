857ee1e9ae471ece634202c5b60d9ccf9114cea1
Oleg Markoff <mawglih@gmail.com>
2021-09-07 18:40:58 -0400

    finished cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw that tree today.',
+    },
+    {
+      title: 'Cool mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature',
+      content: ' Here is a picture of a mountain.',
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking122',
+      content: 'I was riding a bike today',
+    }
+  ]
 }
